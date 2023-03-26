48fcbd53533f0f935880dc2ac0c44640478e9e48
Bryan Jug <bryandjug@gmail.com>
2023-03-25 21:28:15 -0700

    replace build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import './css/style.css';
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-    const { unityProvider } = useUnityContext({
+    const { unityProvider, isLoaded } = useUnityContext({
         loaderUrl: "build/WebGL.loader.js",
         dataUrl: "build/WebGL.data",
         frameworkUrl: "build/WebGL.framework.js",
@@ -12,7 +12,19 @@ function App() {
 
     return (
         <div className="App">
-            <Unity unityProvider={unityProvider} />
+            <Unity 
+                unityProvider={unityProvider}  
+                style={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    top: 0,
+                    left: 0,
+                    margin: "auto",
+                    visibility: isLoaded ? "visible" : "hidden",
+                }}
+                devicePixelRatio={2}
+            />
         </div>
     );
 }
